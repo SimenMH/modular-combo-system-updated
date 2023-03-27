156d31b44a4a9f75566de3bbc769b2d0dd9303cb
Simen H <simenmelo@gmail.com>
2023-03-27 10:58:55 +0200

    Update PlayerCombat.cs

--- PlayerCombat.cs
+++ PlayerCombat.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChainableAttack : MonoBehaviour
+public class PlayerCombat : MonoBehaviour
 {
     public Animator animator;
     public List<AttackSO> attacks;
